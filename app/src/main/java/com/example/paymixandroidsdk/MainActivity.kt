7e4328d5c4dfa4895745865cc4e71fe0cfaef162
naimulNoor <naimul.roni@outlook.com>
2023-05-30 12:31:53 +0600

    init

--- app/src/main/java/com/example/paymixandroidsdk/MainActivity.kt
+++ app/src/main/java/com/example/paymixandroidsdk/MainActivity.kt
@@ -3,15 +3,16 @@ package com.example.paymixandroidsdk
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.toasty.Toaster
+import com.example.toasterjava.Toaster
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val str = com.example.toasterjava.Toaster.getString("Roni")
-        com.example.toasterjava.Toaster.simpleToast(this,str.toString())
+        val str = Toaster.getString("Roni")
+        Toaster.simpleToast(this,str.toString())
+
 
 
     }
